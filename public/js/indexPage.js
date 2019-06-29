ae1ce6ef0c1b6d21db053a6bc3df5da620295b0a
Kumar Shashwat <KSSBro@users.noreply.github.com>
2019-06-29 13:18:20 +0530

    main design done

--- public/js/indexPage.js
+++ public/js/indexPage.js
@@ -3,8 +3,17 @@ var request = new XMLHttpRequest();
 var field = document.getElementById("search-bar");
 var logo = document.getElementById("socialhunter-logo");
 var form = document.getElementById("search-form");
+var cont = document.getElementById("index-search-container");
 
 btn.addEventListener("click", function(event) {
-  logo.classList.add("logo-search-active");
-  form.classList.add("form-search-active");
+  logo.classList.toggle("logo-search-active");
+  logo.classList.toggle("logo-search-inactive");
+  form.classList.toggle("form-search-active");
+  form.classList.toggle("form-search-inactive");
+  cont.classList.toggle("ind-search-cont-search-active");
+  cont.classList.toggle("ind-search-cont-search-inactive");
+  // cont.style.height = "100px";
+  console.log(logo);
+  console.log(form);
+  console.log(cont);
 });
