d18be0592137488307dcab8160896015bf3dcbd1
Kumar Shashwat <KSSBro@users.noreply.github.com>
2019-07-02 21:30:38 +0530

    Implemented instagram hashtag search | Added jquery

--- public/js/indexPage.js
+++ public/js/indexPage.js
@@ -1,19 +1,53 @@
-var btn = document.getElementById("search-btn");
 var request = new XMLHttpRequest();
-var logo = document.getElementById("socialhunter-logo");
-var form = document.getElementById("search-form");
+
 var searchContainer = document.getElementById("index-search-container");
+var mainLogo = document.getElementById("socialhunter-logo");
+var searchForm = document.getElementById("search-form");
+var searchField = document.getElementById("search-field");
+var searchBtn = document.getElementById("search-btn");
 var resContainer = document.getElementById("index-result-container");
 
-btn.addEventListener("click", function(event) {
-  logo.classList.remove("logo-search-inactive");
-  logo.classList.add("logo-search-active");
+searchBtn.addEventListener("click", function(event) {
+  mainLogo.classList.remove("logo-search-inactive");
+  mainLogo.classList.add("logo-search-active");
   searchContainer.classList.remove("ind-search-cont-search-inactive");
   searchContainer.classList.add("ind-search-cont-search-active");
   setTimeout(function() {
     resContainer.classList.remove("ind-res-cont-search-inactive");
     resContainer.classList.add("ind-res-cont-search-active");
   }, 150);
-  // form.classList.toggle("form-search-active");
-  // form.classList.toggle("form-search-inactive");
+  let searchValue = searchField.value;
+  $.ajax({
+    url: "/feed",
+    method: "GET",
+    data: {
+      hashtag: searchValue
+    }
+  }).done(function(data) {
+    resContainer.innerHTML = "";
+    const edges = data["graphql"]["hashtag"]["edge_hashtag_to_media"]["edges"];
+    edges.forEach(function(edge) {
+      resContainer.innerHTML += `
+      <div class="result-card">
+            <img class="result-card-image" src="${
+              edge["node"]["display_url"]
+            }" />
+            <br />
+            <p>
+                Caption: ${
+                  edge["node"]["edge_media_to_caption"]["edges"][0]["node"][
+                    "text"
+                  ]
+                }
+                <br />
+                <br />
+                Likes: ${edge["node"]["edge_liked_by"]["count"]}
+                <br />
+                <br />
+                Comments: ${edge["node"]["edge_media_to_comment"]["count"]}
+            </p>
+        </div>
+        `;
+    });
+  });
 });
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const hbs = require("hbs");
 const path = require("path");
 const bodyParser = require("body-parser");
 const morganLogger = require("morgan");
+const request = require("request");
 
 const port = process.env.PORT || 108;
 const server = express();
@@ -18,6 +19,18 @@ server.use(morganLogger("dev"));
 
 server.get("/", (req, res) => {
   res.status(200).render("indexPage.hbs");
+});
+
+server.get("/feed", (req, res) => {
+  const url = `https://www.instagram.com/explore/tags/${
+    req.query.hashtag
+  }/?__a=1`;
+  request(url, { json: true }, (err, response, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.status(200).send(body);
+  });
 });
 
 server.listen(port, () => {
