facece0cd158a04807be7c2afb69020135e2d36b
Kumar Shashwat <KSSBro@users.noreply.github.com>
2019-07-25 23:32:44 +0530

    update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const morganLogger = require("morgan");
 const request = require("request");
 
 const port = process.env.PORT || 108;
@@ -13,6 +14,7 @@ server.use(
   })
 );
 server.use(bodyParser.json());
+server.use(morganLogger("dev"));
 
 server.get("/", (req, res) => {
   res.status(200).render("indexPage.hbs");
