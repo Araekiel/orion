44c8899ded7cd369c06b503be4842c2f6d57648a
Kumar Shashwat <shashwatdeadcliff@gmail.com>
2019-08-16 14:19:02 +0530

    added err condition to fetchPosts in fetchInstagramData | updated checklist | deploy heroku v40

--- server/modules/fetchInstagramData.js
+++ server/modules/fetchInstagramData.js
@@ -6,7 +6,7 @@ const fetchInstagramData = {
     const url = `https://www.instagram.com/explore/tags/${editedTag}/?__a=1`;
     return new Promise((resolve, reject) => {
       request(url, { json: true }, async (err, response, body) => {
-        if (err) {
+        if (err || !body["graphql"]["hashtag"]) {
           let finalData = [];
           resolve(finalData);
         } else {
