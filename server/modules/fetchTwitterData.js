25f4f60e06c3e1b16a921f7285fb0e63cba5d65a
KSSBro <shashwatdeadcliff@gmail.com>
2019-11-21 02:17:45 +0530

    Started working on twitter API and tweet design

--- server/modules/fetchTwitterData.js
+++ server/modules/fetchTwitterData.js
@@ -2,7 +2,17 @@ const request = require("request");
 
 const fetchTwitterData = {
     fetchTweets: query => {
-        
+        return new Promise((resolve, reject) => {
+            let finalData = [];
+            for(let i = 0; i < 10; i++) {
+                finalData.push({
+                    type: "tweet",
+                    website: "twitter",
+                    htmlString: `<div></div>`
+                });
+            }
+            resolve(finalData);
+        });
     }
 };
 
