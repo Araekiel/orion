f545fa27b7fee8db78d96bf4eddac9b40d56341c
KSSBro <shashwatdeadcliff@gmail.com>
2020-04-17 09:11:02 +0530

    added a new loader

--- public/js/indexPage.js
+++ public/js/indexPage.js
@@ -32,7 +32,8 @@ function search() {
       },
       beforeSend: function() {
         // copyright.style.display = "none";
-        resContainer.innerHTML = `<div class="loader"></div>`;
+        resContainer.innerHTML = `<div class="loader"><div class="loader1"></div><div class="loader2"></div></div>`;
+        console.log(resContainer.innerHTML);
       },
       success: function(data) {
         let resultCardStylesheet = document.getElementById("result-card-stylesheet");
