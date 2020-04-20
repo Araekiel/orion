daeb2b7a4b08c52d7fc8e97744715b85581900a3
KSSBro <shashwatdeadcliff@gmail.com>
2020-04-20 22:26:08 +0530

    removed morgan

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const hbs = require("express-handlebars");
-const morganLogger = require("morgan");
+// const morganLogger = require("morgan");
 const helmet = require("helmet");
 
 const port = process.env.PORT;
