f65ec16ab1f3a506953e08f2ef305f225eb3fde1
KSSBro <shashwatdeadcliff@gmail.com>
2021-06-16 21:50:23 +0530

    fix: removed scrape-twitte import

--- server/modules/fetchTwitterData.js
+++ server/modules/fetchTwitterData.js
@@ -1,40 +1,9 @@
-const { TweetStream, getUserProfile } = require("scrape-twitter");
+// const { TweetStream, getUserProfile } = require("scrape-twitter");
 
 const fetchTwitterData = {
     fetchTweets: (value) => {
         return new Promise((resolve, reject) => {
-            let fetchedData = [];
-            let dataStream = new TweetStream(query=value, type="top", {count:20}); 
-            dataStream.on('data', (dataChunk) => {
-                fetchedData.push({
-                    type: "tweet",
-                    network: "twitter",
-                    data: {
-                        id: dataChunk.id,
-                        username: dataChunk.screenName,
-                        media: {
-                            src: dataChunk.images.length > 0 ? dataChunk.images[0] : ""
-                        },
-                        text: dataChunk.text,
-                        favoriteCount: dataChunk.favoriteCount,
-                        replyCount: dataChunk.replyCount,
-                        retweetCount: dataChunk.retweetCount
-                    }
-                });
-            });
-            dataStream.on("end", () => {
-                // fetchTwitterData.fetchUsers(value).then((printData) => {
-                //     console.log(printData);
-                //     resolve(fetchedData);
-                // }).catch(err => {
-                //     console.log(err);
-                //     resolve(fetchedData);
-                // });
-                resolve(fetchedData);
-            });
-            dataStream.on("error", err => {
-                resolve([]);
-            });
+            resolve([]);
         });
     },
     fetchUsers: (value) => {
