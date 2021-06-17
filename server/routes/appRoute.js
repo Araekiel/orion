686dea0fc27f1b6faeaa30ef91636e93dabfe01c
KSSBro <shashwatdeadcliff@gmail.com>
2021-06-18 01:47:44 +0530

    update: routes/appRoute.js: /img route: acts as a media proxy

--- server/routes/appRoute.js
+++ server/routes/appRoute.js
@@ -1,16 +1,23 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const request = require("request");
 
 const router = express.Router();
 
 const { webFeedProcessor } = require("../modules/webFeedProcessor.js");
 
-router.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
-router.use(bodyParser.json());
+// @route: /img?url="image_url"
+// @desc: This route act as a proxy to get an image from the instagram cdn and send it to the client
+router.get("/img", async(req, res) => {
+  let headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0",
+  }
+  let query = req.query;
+  var url = Object.keys(query).map((key) => `${key}=${query[key]}`).join("&").replace("url=", "");
+  request.get({url:url, headers:headers, encoding:null}, (err, response, body) => {
+    res.set('Content-Type', 'image/jpeg');
+    res.send(body);
+  });
+});
 
 router.get("/webfeed", async (req, res, next) => {
   const value = req.query.value;
